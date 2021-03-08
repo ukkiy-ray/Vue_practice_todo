618399f6af9b9f852f4c27cda577c205b4cc136a
ukkiy-ray <showy13f207ag@gmail.com>
2021-03-08 23:20:47 +0900

    Todoの初期表示確認

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 (function() {
   'use strict';
+
+  // two way data binding(to UI)
+
+  // vm は view model の略
+  // el は elements の略
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      todos: [
+        'task 1',
+        'task 2',
+        'task 3'
+      ]
+    }
+  });
 })();
