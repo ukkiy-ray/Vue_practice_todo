354c399e149b7c17c84001c9ce91439c9de92ca2
ukkiy-ray <showy13f207ag@gmail.com>
2021-03-08 23:57:52 +0900

    新規todo追加機能の実装

--- js/main.js
+++ js/main.js
@@ -8,11 +8,28 @@
   var vm = new Vue({
     el: '#app',
     data: {
+      newItem: '',
       todos: [
         'task 1',
         'task 2',
         'task 3'
       ]
+    },
+    methods: {
+      //  addItem: function(e) {
+      //    e.preventDefault();
+      //    this.todos.push(this.newItem);
+      //  }
+      // e.preventDefault で画面遷移を防いでもいいが、よく行う処理のため html のディレクティブの方で書いても同じ処理になる。
+
+      
+      addItem: function() {
+        this.todos.push(this.newItem);
+        // push することで newItem に追加された内容が todos の末尾に追加されて、結果として html の li 要素に反映される
+
+        this.newItem = '';
+        // input 要素の中を空文字にする
+      }
     }
   });
 })();
