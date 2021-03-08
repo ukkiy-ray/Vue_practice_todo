908d2f4b07e97a4d801596021789ae9ddf2b31f1
ukkiy-ray <showy13f207ag@gmail.com>
2021-03-09 04:13:03 +0900

    todo削除機能実装

--- js/main.js
+++ js/main.js
@@ -22,13 +22,19 @@
       //  }
       // e.preventDefault で画面遷移を防いでもいいが、よく行う処理のため html のディレクティブの方で書いても同じ処理になる。
 
-      
+
       addItem: function() {
         this.todos.push(this.newItem);
         // push することで newItem に追加された内容が todos の末尾に追加されて、結果として html の li 要素に反映される
 
         this.newItem = '';
         // input 要素の中を空文字にする
+      },
+
+      deleteItem: function(index) {
+        if (confirm('are you sure?')) {
+          this.todos.splice(index, 1); // 引数の (index) 番目から1つ削除
+        }
       }
     }
   });
